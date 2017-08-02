ef14cd30c37862ba7adc5cfcdd0d6880018d39ca
Chris Miller <chrismillersd@gmail.com>
2017-08-02 10:54:48 -0700

    Update and rename elasticsearch-5.2.rb to elasticsearch-5.2.2.rb

--- elasticsearch-5.2.2.rb
+++ elasticsearch-5.2.2.rb
@@ -1,4 +1,4 @@
-class Elasticsearch < Formula
+class Elasticsearch522 < Formula
   desc "Distributed search & analytics engine"
   homepage "https://www.elastic.co/products/elasticsearch"
   url "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.2.2.tar.gz"
